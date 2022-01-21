6fe3cfcfff4b7be6d9e958e2443a3d6f1f18bd8a
ZBXpectrum <88217293+ZBXpectrum@users.noreply.github.com>
2022-01-21 11:44:07 +0800

    Add files via upload

---
+++ coursework/Actual Stuff/TIPS.py
@@ -0,0 +1,20 @@
+tips = ["You should create a schedule", #1
+        "If you fail the exam, you have done badly", #2
+        "Is this app even helpful?", #3
+        "You should go outside, or don't", #4
+        "Learn Javascript", #5
+        "What's 2+2? Seriously I don't know", #6
+        "When was the last time you slept?", #7
+        "I need coffee, brb", #8
+        "Dark Mode coming soon with the Pro version", #9
+        "Maybe that degree isn't worth it after all...", #10
+        "Remember to take breaks... If allowed", #11
+        "Work smart, not hard - Someone", #12
+        "In the midst of chaos, there is also opportunity. - Sun Tzu", #13
+        "Make sure you aren't forgetting any homework", #14
+        "7-8 hours of sleep goes a long way", #15
+        "Good morning/afternoon/night/why are you still awake?", #16
+        "Joe was here", #17
+        "ZB was here", #18
+        "Abdullah was not here :(", #19
+        "Water your plants!" #20]
